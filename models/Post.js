d8a76eb7d86f0f3bb17b354f25a97686c92f0c6d
pppreap <pppreap@icloud.net>
2022-10-17 21:30:13 -0400

    added column, define posts

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,40 @@
-const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+class Post extends Model {}
+
+//define tables and columns and specifications
+Post.init(
+  {
+      //defining each column
+      id: {
+          type:DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
+      },
+      title: {
+          type:DataTypes.STRING,
+          allowNull: false,
+      },
+      post_content: {
+          type:DataTypes.TEXT,
+          allowNull: true,
+      },
+      user_id: {
+        type:DataTypes.INTEGER,
+        references: {
+        model: 'user',
+        key: 'id'
+    }
+  }
+},
+{
+  sequelize,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'post'
+}
+);
+
+module.exports= Post;
