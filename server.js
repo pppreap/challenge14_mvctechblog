73d1ba33992ca861d3539f2a56089fe5c1924a8c
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-10-19 19:11:02 -0400

    server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const sess = {
   })
 };
 
-app.use(session(sess));
+
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
@@ -40,6 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session(sess));
 // turn on routes
 app.use(routes);
 
