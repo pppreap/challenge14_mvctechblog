c9b37d5f80e751f48653a577aca939a7996faccd
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-10-19 22:35:18 -0400

    edited handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -20,29 +20,29 @@ if (username && password) {
 
 }
 
-// async function signupFormHandler(event){
-//     event.preventDefault();
+async function signupFormHandler(event){
+    event.preventDefault();
     
-//     const username= document.querySelector(`#username-signup`).value.trim();
-//     const password= document.querySelector(`#password-signup`).value.trim();
+    const username= document.querySelector(`#username-signup`).value.trim();
+    const password= document.querySelector(`#password-signup`).value.trim();
     
-//     if (username && password) {
-//         const response = await fetch('api/users/',{
-//             method:'POST',
-//             body: JSON.stringify({ username, password}),
-//             headers: { 'Content-Type': 'application/json'}
-//         });
+    if (username && password) {
+        const response = await fetch('api/users/',{
+            method:'POST',
+            body: JSON.stringify({ username, password}),
+            headers: { 'Content-Type': 'application/json'}
+        });
     
-//         if (response.ok) {
-//             // document.location.replace('/');
-//             document.location.reload();
-//         } else {
-//             alert(response.statusText);
-//         }
-//     }
+        if (response.ok) {
+            // document.location.replace('/');
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
     
-//     }
+    }
 
     document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-    // document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+    document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
     
