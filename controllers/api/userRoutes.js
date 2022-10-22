d4ed13663e276ffd09abe32240b7433c17e06265
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-10-22 00:44:24 -0400

    edit signupjs

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -59,49 +59,52 @@ router.get('/:id', (req, res) => {
 
 //POST /api/user
 router.post('/', (req, res) => {
-    //access user model and use find one user by id
-         User.Create({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password 
-         })
-         .then(dbUserData => {
-            req.session.save(()=> {
-            req.session.user_id = dbUserData.id;
-            req.session.username = dbUserData.username;
-            req.session.logged_in = true;
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+  .then(dbUserData => {
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.logged_in = true;
   
-            res.json(dbUserData);
-            });
-        });
+      res.json(dbUserData);
     });
+  });
+});
+
 
 //login
-router.post('/login',(req, res) => {
-    //access user model and use find one user by email
-      User.findOne({
-        email: req.body.email     
-    })
-    .then(dbUserData => {
-    if (!dbUserData) {
-        res.status(400).json({message:'Not a valid email'});
+  router.post('/login', (req, res) => {
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
         return;
-    }
-    const validPassword = dbUserData.checkPassword(req.body.password);
-    if (!validPassword) {
-        res.status(400).json({message: "Not a Valid Password!"});
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
         return;
-    }
-
-    req.session.save(()=>{
+      }
+  
+      req.session.save(() => {
         req.session.user_id = dbUserData.id;
         req.session.username = dbUserData.username;
         req.session.logged_in = true;
-
-        res.json({user: dbUserData, message: "Logged In Successfully!"});
-        });
+  
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
     });
- });
+  });
+
 
 router.post('/logout',  (req, res) => {
     if (req.session.logged_in) {
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,16 +1,21 @@
 async function signupFormHandler(event){
     event.preventDefault();
+
     const email = document.querySelector('#email-signup').value.trim();
     const username= document.querySelector('#username-signup').value.trim();
     const password= document.querySelector('#password-signup').value.trim();
     
-    if (email && username && password) {
-        const response = await fetch('/api/users', 
-        {
-            method:'post',
-            body: JSON.stringify({ email, username, password }),
-            headers: { 'Content-Type': 'application/json'}
-        });
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
     
         if (response.ok) {
             document.location.replace('/dashboard');
@@ -21,4 +26,4 @@ async function signupFormHandler(event){
     }
     
     }
-    document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
+    document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
