20f3e0e59c6a8c38f4fbfb50264add61abff2b4c
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-10-22 00:51:30 -0400

    changed render oage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -102,44 +102,6 @@ router.get('/post/:id', (req, res) => {
   });
 });
 
-// router.get('/posts-comments', (req, res) => {
-//   Post.findOne({
-//     where: {
-//       id: req.params.id,
-//     },
-//     attributes: ['id', 'content', 'title', 'created_at'],
-//     include: [
-//       {
-//         model: Comment,
-//         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//         include: {
-//           model: User,
-//           attributes: ['username'],
-//         },
-//       },
-//       {
-//         model: User,
-//         attributes: ['username'],
-//       },
-//     ],
-//   })
-//     .then((dbPostData) => {
-//       if (!dbPostData) {
-//         res.status(404).json({ message: 'No post found with this id' });
-//         return;
-//       }
-//       const post = dbPostData.get({ plain: true });
 
-//       res.render('posts-comments', {
-//         post,
-//         logged_in: req.session.logged_in,
-//         username: req.session.username,
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-// });
 
 module.exports = router;
